3a770dacf37c5e53957b251ec4c82d02e39e1ce4
SalmanAliAkbar <136970020+SalmanAliAkbar@users.noreply.github.com>
2023-10-15 14:33:40 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,97 @@
+// // #Chapter-01:
+
+// // Question No 01: 
+
+// alert("Welcome to the Website");
+
+// // Question No 02:
+
+// alert("Error! Please enter a valid password.");
+
+// // Question No 03:
+
+// alert("Welcome to JS Land...\nHappy Coding!");
+
+// // Question No 04:
+
+// alert("Welcome to JS Land");
+// alert("Happy Coding!\n prevent this page from creating additional dialogs.");
+
+// // Question No 05:
+
+// alert("Hello...I can run JS through my web browser's console");
+
+// // Chapter No 02:
+
+// // Question No 01:
+ 
+// var username;
+
+// // Question No 02:
+
+// var myName="Salman Ali";
+// alert(myName);
+
+// // Question No 03:
+
+// var message="Hello World";
+// alert(message);
+
+// // Question No 04:
+
+// var name="Salman Ali";
+// alert(name);
+// var age=18;
+// alert(age);
+// var diploma="Certified Moblie Application Development";
+// alert(diploma);
+
+// //Question No 05:
+
+// var alertmessage="PIZZA \n PIZZ \n PIZ \n PI \n P";
+// alert(alertmessage);
+
+// // Question No 06:
+
+// var email="salmanali27287@gmail.com";
+// var message="My email address is " + " " + email;
+// alert(message);
+
+// // Question No 07:
+
+// var Book="A smarter\n way to learn JavaScript";
+// var message="I am trying to learn from the Book" + " " + Book;
+// alert(message);
+
+// // Chapter No 03:
+
+// // Question No 01:
+
+// var age=18;
+// alert("I am " + age + "years old.");
+
+// // Question No 02:
+
+// var visitCount = 14; 
+// alert("You have visited this site " + visitCount + " times.");
+
+// // Question No 03:
+
+// var birthYear = "2005";
+// document.write("My birth year is " + birthYear + "<br> Data type of my variable is  number <br>" );
+
+// // Question No 04:
+
+// var visitorName = "John Doe";
+// var productTitle = "T-shirt";
+// var quantity = 5; 
+// document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store.");
+
+// //Chapter No 04:
+
+// // Question No 01:
+
+// var variable1,variable2,variable3;
+
+
+
