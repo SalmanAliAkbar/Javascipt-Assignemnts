1071f298383483e47d505d972686d4d4b3d837c5
SalmanAliAkbar <136970020+SalmanAliAkbar@users.noreply.github.com>
2023-12-16 19:35:53 +0500

    done

---
+++ Even And Odd Number/app.js
@@ -0,0 +1,17 @@
+// // Find Even And Odd Number!!
+
+// let numberArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+// let evenNumber = []
+// let oddNumber = []
+
+// for(let i = 0; i <= numberArray.length; i++){
+//     if(numberArray[i] %2 == 0){
+//         evenNumber.push(numberArray[i])
+//     }
+//     else{
+//         oddNumber.push(numberArray[i])
+//     }
+// }
+
+// document.write("Even Number: " + evenNumber)
+// document.write("<br>" + "Odd Number: " + oddNumber)
--- Palindrome/app.js
+++ Palindrome/app.js
@@ -1,13 +1,13 @@
-// Take Input From User:
-var str = prompt("Please enter a String: ")
+// // Take Input From User:
+// var str = prompt("Please enter a String: ")
 
-// Length Of A String: 
-var len = str.length
-var msg = "It's a palindrome"
+// // Length Of A String: 
+// var len = str.length
+// var msg = "It's a palindrome"
 
-for(var i = 0; i < len/2; i++){
-    if(str[i] != str[len -1 -i]){
-        console.log(str + " It's not a palindrome")
-    }
-}
-console.log(str + " " + msg)
+// for(var i = 0; i < len/2; i++){
+//     if(str[i] != str[len -1 -i]){
+//         console.log(str + " It's not a palindrome")
+//     }
+// }
+// console.log(str + " " + msg)
